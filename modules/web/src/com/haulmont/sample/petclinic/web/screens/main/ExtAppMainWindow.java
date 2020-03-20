0c22e1c9c35b1436c5a6166cd254fae21d873974
Mario <mario@die-davids.de>
2020-03-20 20:46:42 +0100

    added layout; added visit sharing capability

--- modules/web/src/com/haulmont/sample/petclinic/web/screens/main/ExtAppMainWindow.java
+++ modules/web/src/com/haulmont/sample/petclinic/web/screens/main/ExtAppMainWindow.java
@@ -1,24 +1,28 @@
 package com.haulmont.sample.petclinic.web.screens.main;
 
-import com.haulmont.cuba.gui.components.AbstractMainWindow;
-import com.haulmont.cuba.gui.components.Image;
-import com.haulmont.cuba.gui.components.mainwindow.FtsField;
-import java.util.Map;
+import com.haulmont.cuba.gui.components.mainwindow.SideMenu;
+import com.haulmont.cuba.gui.screen.Subscribe;
+import de.diedavids.cuba.userinbox.web.screens.SideMainwindowWithMessages;
+
 import javax.inject.Inject;
+import java.util.Map;
 
-public class ExtAppMainWindow extends AbstractMainWindow {
-    @Inject
-    private FtsField ftsField;
+public class ExtAppMainWindow extends SideMainwindowWithMessages {
+
 
     @Inject
-    private Image logoImage;
+    private SideMenu sideMenu;
 
     @Override
     public void init(Map<String, Object> params) {
         super.init(params);
 
-        initLayoutAnalyzerContextMenu(logoImage);
-        initLogoImage(logoImage);
-        initFtsField(ftsField);
+        sideMenu.getMenuItemNN("messages")
+                .setStyleName("messages-petclinic-style");
+
+
     }
+
+
+
 }
--- modules/web/src/com/haulmont/sample/petclinic/web/visit/visit/VisitBrowse.java
+++ modules/web/src/com/haulmont/sample/petclinic/web/visit/visit/VisitBrowse.java
@@ -1,12 +1,32 @@
 package com.haulmont.sample.petclinic.web.visit.visit;
 
+import com.haulmont.cuba.gui.components.ButtonsPanel;
+import com.haulmont.cuba.gui.components.GroupTable;
+import com.haulmont.cuba.gui.components.Table;
 import com.haulmont.cuba.gui.screen.*;
 import com.haulmont.sample.petclinic.entity.visit.Visit;
+import de.diedavids.cuba.userinbox.web.WithEntitySharingSupport;
+
+import javax.inject.Inject;
 
 @UiController("petclinic_Visit.browse")
 @UiDescriptor("visit-browse.xml")
 @LookupComponent("visitsTable")
 @LoadDataBeforeShow
-public class VisitBrowse extends StandardLookup<Visit> {
+public class VisitBrowse extends StandardLookup<Visit> implements WithEntitySharingSupport {
 
+    @Inject
+    protected GroupTable<Visit> visitsTable;
+    @Inject
+    protected ButtonsPanel buttonsPanel;
+
+    @Override
+    public Table getListComponent() {
+        return visitsTable;
+    }
+
+    @Override
+    public ButtonsPanel getButtonsPanel() {
+        return buttonsPanel;
+    }
 }
